9da01e22661d6df59bfef75d9f64c016b515fdf6
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-03-26 11:47:59 +0530

    R-TK-02

---
+++ React-Redux-Toolkit-01/features/icecream/icecreamSlice.js
@@ -0,0 +1,18 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+const initialState = {
+    numOfIceCream: 20
+}
+const iceCreamSlice = createSlice({
+    name: 'ICECREAM',
+    initialState: initialState,
+    reducers: {
+        ordered: (state) => {
+            state.numOfIceCream--;
+        },
+        restocked: (state, action) => {
+            state.numOfIceCream += action.payload
+        }
+    }
+})
+module.exports = iceCreamSlice.reducer
+module.exports.iceCreamActions = iceCreamSlice.actions
--- React-Redux-Toolkit-01/rtk1.js
+++ React-Redux-Toolkit-01/rtk1.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require("./features/cake/cakeSlice").cakeActions
+const iceCreamActions = require("./features/icecream/icecreamSlice").iceCreamActions
 console.log("Initial State: ",store.getState());
 const unsubscribe = store.subscribe(() => {
     console.log('Update State: ',store.getState());
@@ -10,4 +11,11 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(1))
 store.dispatch(cakeActions.restocked(1))
 store.dispatch(cakeActions.restocked(1))
+
+store.dispatch(iceCreamActions.ordered())
+store.dispatch(iceCreamActions.ordered())
+store.dispatch(iceCreamActions.ordered())
+store.dispatch(iceCreamActions.restocked(1))
+store.dispatch(iceCreamActions.restocked(1))
+store.dispatch(iceCreamActions.restocked(1))
 unsubscribe();
