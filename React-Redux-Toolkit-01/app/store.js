9ceffd749cdadbb41de32674cf7983c034832256
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-03-26 13:24:44 +0530

    R-TK-05

--- React-Redux-Toolkit-01/app/store.js
+++ React-Redux-Toolkit-01/app/store.js
@@ -2,11 +2,13 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 // const reduxLogger = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice');
 const iceCreamReducer = require('../features/icecream/icecreamSlice');
+const userReducer = require('../features/user/userSlice');
 // const logger = reduxLogger.createLogger();
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
-        icecream: iceCreamReducer
+        icecream: iceCreamReducer,
+        user: userReducer,
     },
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
--- React-Redux-Toolkit-01/rtk1.js
+++ React-Redux-Toolkit-01/rtk1.js
@@ -1,21 +1,24 @@
 const store = require('./app/store')
 const cakeActions = require("./features/cake/cakeSlice").cakeActions
 const iceCreamActions = require("./features/icecream/icecreamSlice").iceCreamActions
+const fetchUsers = require("./features/user/userSlice").fetchUsers
 console.log("Initial State: ",store.getState());
 const unsubscribe = store.subscribe(() => {
     console.log('Update State: ',store.getState());
 })
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(1))
-store.dispatch(cakeActions.restocked(1))
-store.dispatch(cakeActions.restocked(1))
+store.dispatch(fetchUsers());
 
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.restocked(1))
-store.dispatch(iceCreamActions.restocked(1))
-store.dispatch(iceCreamActions.restocked(1))
-unsubscribe();
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(1))
+// store.dispatch(cakeActions.restocked(1))
+// store.dispatch(cakeActions.restocked(1))
+
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.restocked(1))
+// store.dispatch(iceCreamActions.restocked(1))
+// store.dispatch(iceCreamActions.restocked(1))
+// unsubscribe();
